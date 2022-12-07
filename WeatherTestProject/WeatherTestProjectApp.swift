88b9413ac5811fdd5ea28f0c887d3420c733ea97
Andrew <AndSwiftUs@gmail.com>
2022-12-07 16:29:44 +0300

    Initial Commit

---
+++ WeatherTestProject/WeatherTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTestProjectApp.swift
+//  WeatherTestProject
+//
+//  Created by Andrew Us on 07.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
